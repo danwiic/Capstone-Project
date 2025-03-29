47d9f27734c80b272f8cb2633aac95a99d49db61
Dan Pirante <148037389+danwiic@users.noreply.github.com>
2025-03-29 19:00:27 +0800

    v2

--- frontend/src/ui/CategoriesCard.tsx
+++ frontend/src/ui/CategoriesCard.tsx
@@ -5,12 +5,13 @@ type CategoryProps = {
 
 export default function CategoriesCard({ image, label }: CategoryProps) {
   return (
-    <div className="p-4 w-51 rounded-xs bg-white cursor-pointer shadow:sm hover:shadow-md flex flex-col gap-4 max-w-51">
+    <div className=" lg:w-42 xl:w-51 1400:w-55 rounded-xs 
+        bg-white cursor-pointer shadow:sm hover:shadow-md flex flex-col gap-4 min-w-45 h-46 justify-center items-center p-4">
       {image && (
         <img
           src={image}
           alt="Category"
-          className="w-full h-32 object-cover rounded-md"
+          className=" max-w-30 max-h-30 object-cover rounded-md"
         />
       )}
       {label && <span className="mt-2 text-center text-lg">{label}</span>}
