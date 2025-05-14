1e4a15dedf7e42f5df11ac14239bbeb4799aa8fa
Dan Pirante <148037389+danwiic@users.noreply.github.com>
2025-05-14 19:10:33 +0800

    s

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -2,17 +2,27 @@ import { useState } from "react";
 import Footer from "../components/footer/Footer";
 import Login from "./Login";
 import Signup from "./Signup";
+import { Link } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
 
 const tabs = [
   { name: "login", component: <Login /> },
   { name: "signup", component: <Signup /> },
 ];
 
-
 export default function SignIn() {
   const [activeTab, setActiveTab] = useState("login");
   return (
     <>
+      <div className="p-4">
+        <Link
+          to={"/"}
+          className="flex items-center gap-2 bg-mayormoto-pink px-4 py-2 w-fit
+          text-white font-medium rounded text-sm uppercase"
+        >
+          <ArrowLeft /> Back
+        </Link>
+      </div>
       <div className="flex items-center justify-center w-full p-20">
         <div className="w-1/3 p-1 bg-white rounded-lg shadow-1">
           <div
